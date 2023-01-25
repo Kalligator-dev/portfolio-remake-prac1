26b477de2ec58dc7eab0f78f6d08a4ad8c69f3d5
Abdullah <kalli@kalligator.com>
2023-01-25 15:00:08 +0200

    v0.2 added a counter and refactored current code a little ... used wider images to showcase the parallax effect better

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-import { animateTrack } from "./helpers.js";
+import { animateTrack, animateCounter } from "./helpers.js";
 
 const track = document.getElementById("image-track");
 const counterEl = document.getElementById("counter");
@@ -7,10 +7,9 @@ const totalEl = document.getElementById("total-count");
 const total = track.childElementCount;
 const imgPrcFr = 50 / total;
 let currentImage = 1;
-counterEl.innerText = currentImage;
 totalEl.innerText = total;
 
-let freeScroll = false;
+let freeScroll = true;
 
 if (!freeScroll) {
   track.style.opacity = 0;
@@ -20,15 +19,47 @@ if (!freeScroll) {
     { opacity: 1 },
     {
       duration: 1200,
-      delay: 500,
       fill: "forwards",
       easing: "cubic-bezier(0.550, 0.085, 0.680, 0.530)",
     }
   );
 }
 
+for (let i = 0; i < total; i++) {
+  const countDiv = document.createElement("div");
+  countDiv.className = "count-text";
+  countDiv.innerText = +i + 1;
+  counterEl.appendChild(countDiv);
+}
+
+let currentPrc,
+  rep = 0;
+let animationDone;
+const trackWidth = track.offsetWidth;
+function updateValue() {
+  const style = window.getComputedStyle(track);
+  const matrix = new WebKitCSSMatrix(style.transform);
+  const currentTransform = -(matrix.m41 * 100) / trackWidth;
+  const curIndex = Math.floor((currentTransform * total) / 100);
+  if (currentImage !== curIndex + 1) {
+    currentImage = curIndex + 1;
+    animateCounter((Math.min(curIndex, total - 1) * 100) / total);
+  }
+
+  if (currentPrc == currentTransform && animationDone) {
+    rep++;
+  } else {
+    rep = 0;
+    currentPrc = currentTransform;
+  }
+  if (rep > 10) return;
+  requestAnimationFrame(updateValue);
+}
+
 const handleMouseDown = (e) => {
   track.dataset.mouseDownAt = e.clientX;
+  animationDone = false;
+  requestAnimationFrame(updateValue);
 };
 const handleMouseUp = () => {
   track.dataset.mouseDownAt = "0";
@@ -38,6 +69,7 @@ const handleMouseUp = () => {
     track.dataset.prevPercentage = prc;
     animateTrack(prc, 600);
   }
+  animationDone = true;
 };
 const handleDrag = (e) => {
   if (track.dataset.mouseDownAt === "0") return;
@@ -47,8 +79,8 @@ const handleDrag = (e) => {
   const prev = parseFloat(track.dataset.prevPercentage);
   const cur = prev + prc;
   const constrainedPrc = Math.min(Math.max(cur, 0), 100);
-  const currentImage = Math.max(Math.ceil(constrainedPrc / (imgPrcFr * 2)), 1);
-  counterEl.innerText = currentImage;
+  const floatedCurrentImage = Math.max(constrainedPrc / (imgPrcFr * 2), 1);
+  const currentImage = Math.ceil(floatedCurrentImage);
   track.dataset.currentImage = currentImage;
 
   track.dataset.percentage = constrainedPrc;
@@ -56,10 +88,8 @@ const handleDrag = (e) => {
     track.dataset.mouseDownAt = e.clientX;
     track.dataset.prevPercentage = constrainedPrc;
   }
-
   animateTrack(constrainedPrc);
 };
-
 // mouse events
 window.onmousedown = (e) => handleMouseDown(e);
 window.onmouseup = (e) => handleMouseUp(e);
--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,4 +1,5 @@
 const track = document.getElementById("image-track");
+const counterEl = document.getElementById("counter");
 
 export const animateTrack = (
   progress,
@@ -29,3 +30,58 @@ export const animateTrack = (
     );
   }
 };
+
+export const animateCounter = (progress, duration = 200, easingProgress) => {
+  counterEl.animate(
+    {
+      transform: `translate(0, ${-progress}%)`,
+    },
+    {
+      duration: duration,
+      fill: "forwards",
+    }
+  );
+};
+
+export function getTranslateValues(element) {
+  const style = window.getComputedStyle(element);
+  const matrix = style.transform || style.webkitTransform || style.mozTransform;
+
+  // No transform property. Simply return 0 values.
+  if (matrix === "none") {
+    return {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+  }
+
+  // Can either be 2d or 3d transform
+  const matrixType = matrix.includes("3d") ? "3d" : "2d";
+  const matrixValues = matrix.match(/matrix.*\((.+)\)/)[1].split(", ");
+
+  // 2d matrices have 6 values
+  // Last 2 values are X and Y.
+  // 2d matrices does not have Z value.
+  if (matrixType === "2d") {
+    return {
+      x: matrixValues[4],
+      y: matrixValues[5],
+      z: 0,
+    };
+  }
+
+  // 3d matrices have 16 values
+  // The 13th, 14th, and 15th values are X, Y, and Z
+  if (matrixType === "3d") {
+    return {
+      x: matrixValues[12],
+      y: matrixValues[13],
+      z: matrixValues[14],
+    };
+  }
+}
+
+export function easeInQuad(time, b, c, d) {
+  return c * (time /= d) * time + b;
+}
